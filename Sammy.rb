e6efb39177173b2236aa412c47e79200f6ec0847
sammy6345 <sammy6345@users.noreply.github.com>
2017-04-01 23:06:18 +1300

    Added new commands.
    New commands added but I broke the bot somehow. Not sure why it won't let me run it like this but I'll figure it out later.

--- Sammy.rb
+++ Sammy.rb
@@ -5,15 +5,16 @@ require_relative 'config.rb'
 
 bot = Discordrb::Commands::CommandBot.new token: configatron.token , client_id: 271228733749329920, prefix: '!$'
 
+#bot.update_status(status, game="Type !$ for help")
 
 bot.message(content: 'Ping!') do |event|
   m = event.respond('Pong!')
   m.edit("Pong! Time taken: #{Time.now - event.timestamp} seconds.")
   end
 
-#bot.message(content: 'Invite me') do |event|
-#  event.respond("My invite link is https://discordapp.com/api/oauth2/authorize?client_id=271228733749329920&scope=bot&permissions=0")
-#  end
+bot.command(:invite) do |event|
+  event.respond("My invite link is https://discordapp.com/api/oauth2/authorize?client_id=271228733749329920&scope=bot&permissions=0")
+  end
 
 bot.command(:eval, help_available: false) do |event, *code|
   break unless event.user.id == 138116156488679425
@@ -33,6 +34,15 @@ end
 #bot.command(:help) do |event|
 #  event << 
 
-bot.run :async
+bot.command(:purge) do |event, rm_arg|
+  while rm_arg > 100 do
+    rm_messages = channel.history(100)
+    message_no -= 100
+  end
+  rm_messgaes = channel.history(rm_arg)  
+  event.message.delete(rm_messages)
+
+bot.run  :async
 #print "Bot is now running"
-bot.sync 
+bot.sync
+
